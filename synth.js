adab97bb940adf729436922ae47e690169727e07
Tristan Matthias <hello@tristanmatthias.com>
2020-10-08 18:47:37 -0500

    feat: hooked up piano

--- synth.js
+++ synth.js
@@ -1,1 +1,22 @@
+let synth;
+
+// Workaround for the audio context warning
+const start = () => {
+  window.removeEventListener('click', start);
+
+  //create a synth and connect it to the main output (your speakers)
+  synth = new Tone.Synth().toDestination();
+
+  // Register the buttons to play the notes
+  document.querySelectorAll('[data-key]').forEach(k => {
+    const key = k.getAttribute('data-key');
+    k.addEventListener('click', () => playNote(key));
+  });
+};
+window.addEventListener('click', start);
+
+
+const playNote = (key) => {
+  synth.triggerAttackRelease(key, '8n');
+}
 
