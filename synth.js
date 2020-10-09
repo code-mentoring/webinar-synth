cdc04b2836f105687aac713c475f9653781ac10e
Tristan Matthias <hello@tristanmatthias.com>
2020-10-08 19:04:50 -0500

    feat: added keyboard keyboard

--- synth.js
+++ synth.js
@@ -24,3 +24,29 @@ const playNote = (key) => {
   synth.triggerAttackRelease(key, '8n');
 }
 
+
+
+const keyMap = {
+  "a" :"c4",
+  "w" :"c#4",
+  "s" :"d4",
+  "e" :"d#4",
+  "d" :"e4",
+  "f" :"f4",
+  "t" :"f#4",
+  "g" :"g4",
+  "y" :"g#4",
+  "h" :"a4",
+  "u" :"a#4",
+  "j" :"b4",
+  "k" :"c5",
+  "o" :"c#5",
+  "l" :"d5",
+  "p" :"d#5",
+  ";" :"e5",
+  "'" :"f5",
+}
+
+document.addEventListener('keydown', (e) => {
+  if (keyMap[e.key]) playNote(keyMap[e.key]);
+});
